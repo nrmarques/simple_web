cb26acaee169c1b6cf5ff5cadacbf95b55088840
Nuno Marques <nuno.slmarques@gmail.com>
2022-11-01 23:50:21 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -16,27 +16,3 @@ provider "oci" {
   region           = var.region
 }
 
-# ------ Home Region Provider
-data "oci_identity_region_subscriptions" "HomeRegion" {
-    tenancy_id = var.tenancy_ocid
-    filter {
-        name = "is_home_region"
-        values = [true]
-    }
-}
-locals {
-    home_region = lookup(element(data.oci_identity_region_subscriptions.HomeRegion.region_subscriptions, 0), "region_name")
-}
- 
-provider "oci" {
-    alias            = "home_region"
-    tenancy_ocid     = var.tenancy_ocid
-    user_ocid        = var.user_ocid
-    fingerprint      = var.fingerprint
-    private_key_path = var.private_key_path
-    region           = local.home_region
-}
-
-output "Home_Region_Name" {
- value = local.home_region
-}
